4710a457152ca84551df238100c3e8eed1395c09
tokob9999 <96751911+tomkob9999@users.noreply.github.com>
2024-09-06 14:35:26 +0900

    1.0.2

--- percentile_sort.py
+++ percentile_sort.py
@@ -1,8 +1,8 @@
 #
 # percentile_sort
 #
-# Description: Divide and Conquert Sort that uses percentiles instead of ranks to avoid individual comparison and save merge workload unlike quick sort or merge sort
-# Version: 1.0.1
+# Description: a divide and conquert sort algorithm that splits by square root percentiles instead of ranks
+# Version: 1.0.2
 # Author: Tomio Kobayashi
 # Last Update: 2024/9/6
 
@@ -22,14 +22,8 @@ def percentile_sort(arr):
             arr[1] = arr[0]
             arr[0] = a
         return arr
-#     else:
-#         f = arr[0]
-#         if all(a == f for a in arr[1:]):
-#             return arr
-#     print("arr", arr)
 
     # Step 1: Find the min and max
-#     min_val, max_val = find_min_max(arr)
     min_val, max_val = min(arr), max(arr)
 
     # Step 2: If all elements are the same, return the array
@@ -38,40 +32,26 @@ def percentile_sort(arr):
 
     # Step 3: Determine the number of sub-vectors (size log(n))
     n = len(arr)
-#     num_buckets = max(1, int(math.log2(n)))  # Using log2(n) as the number of sub-vectors
-#     num_buckets = max(2, int(math.log2(n)))  # Using log2(n) as the number of sub-vectors
-    num_buckets = max(2, int(math.sqrt(n)))  # Using log2(n) as the number of sub-vectors
+
+    num_buckets = max(2, int(math.sqrt(n)))  # Using square_root(n) as the number of sub-vectors
     
-#     print("math.log2(n)", math.log2(n))
     buckets = [[] for _ in range(num_buckets)]
 
     # Step 4: Insert elements into corresponding buckets based on percentile
     for value in arr:
         # Calculate the bucket index based on the value's percentile between min and max
-#         if max_val == min_val:
-#             bucket_index = 0
-#         else:
-#             bucket_index = min(num_buckets - 1, int((value - min_val) / (max_val - min_val) * num_buckets))
         bucket_index = min(num_buckets - 1, int((value - min_val) / (max_val - min_val) * num_buckets))
         buckets[bucket_index].append(value)
 
-    # Step 5: Recursively sort each bucket
     if sum([1 for b in buckets if len(b) > 0]) == 1:
         return [b for b in buckets if len(b) > 0][0]
 
+    # Step 5: Merge buckets
     sorted_buckets = []
     for bucket in buckets:
         if bucket:
-#             sorted_buckets.append(percentile_sort(bucket))
             sorted_buckets.extend(percentile_sort(bucket))
     return sorted_buckets
-
-#     # Step 6: Merge sorted buckets
-#     result = []
-#     for bucket in sorted_buckets:
-#         result.extend(bucket)
-    
-#     return result
 
 # Example usage
 n = 16  # Size of the vector
