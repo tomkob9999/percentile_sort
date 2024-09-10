b7ec55bc99f53ac2f0422b398f9de638e8575417
tokob9999 <96751911+tomkob9999@users.noreply.github.com>
2024-09-10 09:01:03 +0900

    1.0.9
    - range search is removed as the performance is not practical

--- percentile_sort.py
+++ percentile_sort.py
@@ -4,7 +4,7 @@
 # Description: a divide and conquer sort algorithm that splits by square root percentiles instead of ranks
 # This generatates a Van Emde Boas like tree that is accessible with O(loglogn) as by-product
 #
-# Version: 1.0.8
+# Version: 1.0.9
 # Author: Tomio Kobayashi
 # Last Update: 2024/9/10
 
@@ -23,6 +23,8 @@ class btre:
         return self.searchme(v, self)
     
     def searchme(self, v, bb):
+        if bb.len == -1:
+            return False
         if bb.value1 is not None:
             if v == bb.value1 or v == bb.value2:
                 return True
@@ -34,65 +36,70 @@ class btre:
             return False
         else:
             return self.searchme(v, bb.children[ind])
-    
-    def search_from(self, s):
-        vals = []
-        self.searchme_from(self, s, vals)
-        return vals
         
-    def searchme_from(self, bb, s, vals):
-        if bb.value1 is not None:
-            if bb.value1 >= s:
-                vals.append(bb.value1)
-                if bb.value2 is not None and bb.value2 >= s:
-                    vals.append(bb.value2)
-                return
-        else:
-            if s > bb.max:
-                return
-            else:
-                for b in bb.children:
-                    self.searchme_from(b, s, vals)
+# Rannge search is too slow because it walks 
+#     def search_from(self, s):
+#         vals = []
+#         self.searchme_from(self, s, vals)
+#         return vals
+        
+#     def searchme_from(self, bb, s, vals):
+#         if bb.len == -1:
+#             return False
+#         if bb.value1 is not None:
+#             if bb.value1 >= s:
+#                 vals.append(bb.value1)
+#                 if bb.value2 is not None and bb.value2 >= s:
+#                     vals.append(bb.value2)
+#                 return
+#         else:
+#             if s > bb.max:
+#                 return
+#             else:
+#                 for b in bb.children:
+#                     self.searchme_from(b, s, vals)
 
-    def search_to(self, s):
-        vals = []
-        self.searchme_to(self, s, vals)
-        return vals
+#     def search_to(self, s):
+#         vals = []
+#         self.searchme_to(self, s, vals)
+#         return vals
     
-    def searchme_to(self, bb, s, vals):
-        if bb.value1 is not None:
-            if bb.value1 <= s:
-                vals.append(bb.value1)
-                if bb.value2 is not None and bb.value2 <= s:
-                    vals.append(bb.value2)
-                return
-        else:
-            if s < bb.min:
-                return
-            else:
-                for b in bb.children:
-                    self.searchme_to(b, s, vals)
-                    
+#     def searchme_to(self, bb, s, vals):
+#         if bb.len == -1:
+#             return False
+#         if bb.value1 is not None:
+#             if bb.value1 <= s:
+#                 vals.append(bb.value1)
+#                 if bb.value2 is not None and bb.value2 <= s:
+#                     vals.append(bb.value2)
+#                 return
+#         else:
+#             if s < bb.min:
+#                 return
+#             else:
+#                 for b in bb.children:
+#                     self.searchme_to(b, s, vals)
     
-    def search_range(self, s, f):
-        vals = []
-        self.searchme_range(self, s, f, vals)
-        return vals
+#     def search_range(self, s, f):
+#         vals = []
+#         self.searchme_range(self, s, f, vals)
+#         return vals
         
-    def searchme_range(self, bb, s, f, vals):
-        if bb.value1 is not None:
-            if bb.value1 >= s and bb.value1 <= f:
-                vals.append(bb.value1)
-                if bb.value2 is not None and bb.value2 >= s and bb.value2 <= f:
-                    vals.append(bb.value2)
-                return
-        else:
-            if s > bb.max or f < bb.min:
-                return
-            else:
-                for b in bb.children:
-                    self.searchme_range(b, s, f, vals)
-                    
+#     def searchme_range(self, bb, s, f, vals):
+#         if bb.len == -1:
+#             return False
+#         if bb.value1 is not None:
+#             if bb.value1 >= s and bb.value1 <= f:
+#                 vals.append(bb.value1)
+#                 if bb.value2 is not None and bb.value2 >= s and bb.value2 <= f:
+#                     vals.append(bb.value2)
+#                 return
+#         else:
+#             if s > bb.max or f < bb.min:
+#                 return
+#             else:
+#                 for b in bb.children:
+#                     self.searchme_range(b, s, f, vals)
                     
 import numpy as np
 import math
@@ -138,21 +145,21 @@ def percentile_sort(arr, bb=None):
     # Step 5: Merge buckets
     sorted_buckets = []
     for bucket in buckets:
-        if bucket:
-            if bb is None :
-                sorted_buckets += percentile_sort(bucket)
-            else:
-                bb.children.append(btre())
-                sorted_buckets += percentile_sort(bucket, bb.children[-1])
+#         if bucket:
+        if bb is None :
+            sorted_buckets += percentile_sort(bucket)
+        else:
+            bb.children.append(btre())
+            sorted_buckets += percentile_sort(bucket, bb.children[-1])
     return sorted_buckets
 
 # Example usage
 # n = 16  # Size of the vector
 # vector = np.random.randint(1, 101, size=n)  # Generate a random vector of integers between 1 and 100
-n = 100000  # Size of the vector
-vector = np.random.randint(1, n, size=n)  # Generate a random vector of integers between 1 and 100
-# n = 1000000
-# vector = np.random.random(size=n) * n
+# n = 1000000  # Size of the vector
+# vector = np.random.randint(1, n, size=n)  # Generate a random vector of integers between 1 and 100
+n = 500000
+vector = np.random.random(size=n) * n
 
 print("Original vector:", vector[:50])
 
@@ -187,4 +194,3 @@ print(f"Execution Time: {air_time:.6f} seconds")
 #     ret = bbb.search(i)
 #     if ret:
 #         print("bbb ", i, bbb.search(i))
-
