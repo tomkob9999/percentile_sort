a88855286438dc0d3570e11db22c06230cd00986
tokob9999 <96751911+tomkob9999@users.noreply.github.com>
2024-09-17 07:32:32 +0900

    1.2.9
    - removed link parameter
    - renewed sample executions

---
+++ p_sort.py
@@ -0,0 +1,210 @@
+#
+# p_sort
+#
+# Description: a divide and conquer sort algorithm that splits by square root percentiles instead of ranks
+# This optionally generatates a Van Emde Boas like tree that is accessible with O(loglogn) as by-product
+#
+# Version: 1.2.9
+# Author: Tomio Kobayashi
+# Last Update: 2024/9/17
+
+import numpy as np
+import math
+
+# # Function to find the minimum and maximum of a vector
+# Recursive function to split the array into vectors and merge them
+class p_sort:
+    
+    ROOT_POWER = 1.25 # n^(1/1.25) seems faster than n^(1/2)(=square root)
+    deepest = 0
+    
+    class btre:
+        def __init__(self):
+            self.len = -1
+            self.min = -np.inf
+            self.max = np.inf
+            self.children = []
+            
+            self.pos1 = -1
+            self.pos2 = -1
+            self.sorted_set = None
+
+        def search(self, v, return_node=False):
+            return self.searchme(v, self, return_node)
+        
+        def searchme(self, v, bb, return_node=False):
+            if bb.len == -1:
+                return -1 if not return_node else None
+            if bb.len == 0:
+                if v == bb.min:
+                    return bb.pos1 if not return_node else bb
+                elif v == bb.max:
+                    return bb.pos2 if not return_node else bb
+                else:
+                    return -1 if not return_node else None
+            num_buckets = max(2, int(bb.len**(1/p_sort.ROOT_POWER)))
+            ind = min(num_buckets - 1, int((v - bb.min) / (bb.max - bb.min) * num_buckets))
+            if  ind < 0 or ind > len(bb.children)-1:
+                return -1 if not return_node else None
+            else:
+                return self.searchme(v, bb.children[ind], return_node)
+                    
+        def link(self, sorted_vector):
+            prev = np.inf
+            for i, v in enumerate(sorted_vector):
+                if prev != sorted_vector[i]:
+                    bb = self.search(sorted_vector[i], return_node=True)
+                    if sorted_vector[i] == bb.min:
+                        bb.pos1 = i
+                    elif sorted_vector[i] == bb.max:
+                        bb.pos2 = i
+                    prev = sorted_vector[i]
+            self.sorted_set = sorted_vector
+                
+        def search_from(self, s):
+            suc = self.succ(s)
+            if suc == -1:
+                return []
+            return self.sorted_set[suc:]
+        
+        def search_to(self, f):
+            pre = self.prec(f)
+            if pre == -1:
+                return []
+            return self.sorted_set[:pre+1]
+        
+        def search_range(self, s, f):
+            suc = self.succ(s)
+            pre = self.prec(f)
+            if pre < suc or suc == -1 or pre == -1:
+                return []
+            return self.sorted_set[suc:pre+1]
+            
+
+        def succ(self, s):
+            return self.succme(self, s)
+            
+        def succme(self, bb, s):
+            if bb.len == -1:
+                    return -1                
+            if bb.len == 0:
+                if s <= bb.min:
+                    return bb.pos1
+                elif s <= bb.max:
+                    return bb.pos2
+                else:
+                    return -1
+            else: 
+                num_buckets = max(2, int(bb.len**(1/p_sort.ROOT_POWER)))
+                ind = min(num_buckets - 1, int((s - bb.min) / (bb.max - bb.min) * num_buckets))
+                if ind > len(bb.children)-1:
+                    return -1
+                for i in range(max(0, ind), len(bb.children), 1):
+                    pos = self.succme(bb.children[i], s)
+                    if pos > -1:
+                        return pos
+                return -1
+
+        def prec(self, f):
+            return self.precme(self, f)
+            
+        def precme(self, bb, s):
+            if bb.len == -1:
+                    return -1                
+            if bb.len == 0:
+                if s >= bb.max:
+                    return bb.pos2
+                elif s >= bb.min:
+                    return bb.pos1
+                else:
+                    return -1
+            else: 
+                num_buckets = max(2, int(bb.len**(1/p_sort.ROOT_POWER)))
+                ind = min(num_buckets - 1, int((s - bb.min) / (bb.max - bb.min) * num_buckets))
+                if ind < 0:
+                    return -1
+                for i in range(min(len(bb.children)-1, ind), -1, -1):
+                    pos = self.precme(bb.children[i], s)
+                    if pos > -1:
+                        return pos
+                return -1
+            
+        def update(self, inserts=[], deletes=[]):
+            c = self.sorted_set[0:]
+            for d in deletes:
+                c.remove(d)
+            for i in inserts:
+                c.append(i)
+            return p_sort.sort(c, create_btre=True, link=True)
+            
+    def sort(arr, create_btre=False, find_depth=True):
+        depth = -1
+        p_sort.deepest = 0
+        if find_depth:
+            depth = 0
+            
+        if create_btre:
+            bb = p_sort.btre()
+            return p_sort.percentile_sort(arr, bb, depth=depth), bb
+        else:
+            return p_sort.percentile_sort(arr, depth=depth)
+        
+    def percentile_sort(arr, bb=None, depth=-1):
+        if depth > -1:
+            depth += 1
+            if depth > p_sort.deepest:
+                p_sort.deepest = depth
+        
+        if len(arr) <= 1:
+            if bb is not None and len(arr) == 1:
+                bb.min = arr[0]
+                bb.len = 0
+            return arr
+        elif len(arr) == 2:
+            if arr[0]>arr[1]:
+                arr[0], arr[1] = arr[1], arr[0]
+
+            if bb is not None:
+                if arr[0] == arr[1]:
+                    bb.min = arr[0]
+                else:
+                    bb.min = arr[0]
+                    bb.max = arr[1]
+                bb.len = 0
+            return arr
+
+        # Find the min and max
+        min_val, max_val = min(arr), max(arr)
+        if min_val == max_val:
+            if bb is not None:
+                bb.min = arr[0]
+                bb.len = 0
+            return arr
+
+        if bb is not None:
+            bb.len = len(arr)
+            bb.min = min_val
+            bb.max = max_val
+
+        num_buckets = max(2, int(len(arr)**(1/p_sort.ROOT_POWER)))
+        buckets = [[] for _ in range(num_buckets)]
+
+        # Insert elements into corresponding buckets based on percentile
+        for value in arr:
+            # Calculate the bucket index based on the value's percentile between min and max
+            buckets[min(num_buckets - 1, int((value - min_val) / (max_val - min_val) * num_buckets))].append(value)
+
+        # Merge buckets
+        sorted_buckets = []
+        for bucket in buckets:
+            if bb is None :
+                sorted_buckets += p_sort.percentile_sort(bucket, depth=depth)
+            else:
+                bb.children.append(p_sort.btre())
+                sorted_buckets += p_sort.percentile_sort(bucket, bb.children[-1], depth=depth)
+
+        if bb is not None:
+            bb.link(list(set(sorted_buckets)))
+
+        return sorted_buckets
+    
--- sample_execution.py
+++ sample_execution.py
@@ -1,83 +1,28 @@
-# Example usage
-n = 100  # Size of the vector
-vector = np.random.randint(1, 101, size=n)  # Generate a random vector of integers between 1 and 100
-# # vector = np.random.random(size=n) * n
-# n = 100000  # Size of the vector
-# vector = np.random.randint(1, n, size=n)  # Generate a random vector of integers between 1 and 100
-# n = 100000
-# vector = np.random.random(size=n) * n
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    
+    while left <= right:
+        mid = left + (right - left) // 2
+        
+        # Check if target is present at mid
+        if arr[mid] == target:
+            return mid
+        # If target is greater, ignore the left half
+        elif arr[mid] < target:
+            left = mid + 1
+        # If target is smaller, ignore the right half
+        else:
+            right = mid - 1
+    
+    # If the target is not found
+    return -1
 
-print("Original vector:", vector[:50])
 
-
-# # Perform the recursive sort
+import numpy as np
 import time
-print("p_sort")
-start_time = time.time()
-sorted_vector, bbb = p_sort.sort(vector.tolist(), create_btre=True, link=True)
-# sorted_vector = p_sort.sort(vector.tolist(), create_btre=False, link=True)
-air_time = time.time() - start_time
-print(f"Execution Time: {air_time:.6f} seconds")
+import random
 
-
-# print("Sorted vector:")
-print("Percentile sorted vector:")
-print(sorted_vector[:50])
-# print(sorted_vector)
-print("len(sorted_vector)", len(sorted_vector))
-
-ret = bbb.search(8)
-print("ret", ret)
-
-
-ret = bbb.search(20)
-print("ret", ret)
-print("sorted_set", bbb.sorted_set[ret])
-ret = bbb.succ(24)
-# print("ret", ret)
-print("sorted_set", bbb.sorted_set[ret])
-ret = bbb.prec(29)
-print("ret", ret)
-print("sorted_set", bbb.sorted_set[ret])
-ret = bbb.search_range(22, 40)
-print("ret", ret)
-ret = bbb.search_from(22)
-print("ret", ret)
-ret = bbb.search_to(41)
-print("ret", ret)
-
-new, bb2 = bbb.update([101])
-print("new", new)
-
-new, bb3 = bb2.update([105])
-print("new", new)
-
-new, bb3 = bb3.update([], [101])
-
-
-#######
-# sample test data creation
-#
-
-# Create a vector of random integers between 1 and 1000
-# vector_size = 20000000
-# vector = np.random.randint(1, vector_size, size=vector_size)
-
-# vector_size = 1000000
-# vector = np.random.randint(1, vector_size, size=vector_size)
-
-# Test with skewed data
-# scale = 10000.0  # The scale parameter (1/λ). Lower values increase the skewness.
-# vector = np.random.exponential(scale, n)
-# mean = 1000
-# std_dev = 1
-# vector = np.random.normal(mean, std_dev, n)
-# shape, scale = 0.5, 1.0  # Shape controls the skewness; scale adjusts the spread
-# vector = np.random.gamma(shape, scale, n)
-
-
-# Quick sort implementation used in testin
-
+# Function to implement randomized quicksort
 def quick_sort(arr):
     if len(arr) <= 1:
         return arr
@@ -89,3 +34,73 @@ def quick_sort(arr):
         return quick_sort(less) + equal + quick_sort(greater)
 
 
+
+
+vector_size = 1000000
+vector = np.random.randint(1, vector_size, size=vector_size)
+# vector = np.random.random(size=vector_size) * vector_size + 1
+# shape_param = 1.1
+# vector = np.random.pareto(shape_param, vector_size)
+
+
+import time
+start_time = time.time()
+v = np.log(vector)
+sorted_dat = p_sort.sort(v)
+sorted_dat = np.exp(sorted_dat)
+percentile_sort_time = time.time() - start_time
+# print("sorted_dat", sorted_dat[:20])
+print(f"P Sort Time with log: {percentile_sort_time:.6f} seconds")
+print("p_sort.deepest", p_sort.deepest)
+
+start_time = time.time()
+sorted_dat = p_sort.sort(vector)
+percentile_sort_time = time.time() - start_time
+# print("sorted_dat", sorted_dat[:20])
+print(f"P Sort Time: {percentile_sort_time:.6f} seconds")
+print("p_sort.deepest", p_sort.deepest)
+
+
+
+
+vector_size = 1000000
+vector = np.random.randint(1, vector_size, size=vector_size)
+# vector = np.random.random(size=vector_size) * vector_size + 1
+# shape_param = 1.1
+# vector = np.random.pareto(shape_param, vector_size)
+
+import time
+start_time = time.time()
+sorted_vector, bbb = p_sort.sort(vector, create_btre=True)
+percentile_sort_time = time.time() - start_time
+# print("sorted_dat", sorted_dat[:20])
+print(f"P Sort Time: {percentile_sort_time:.6f} seconds")
+print("p_sort.deepest", p_sort.deepest)
+
+siz = int(vector_size/10)
+
+
+# vec = list(set(sorted_vector))
+print("Binary Search")
+start_time = time.time()
+for i in range(1, siz, 1):
+    ret = binary_search(sorted_vector, i)
+#     ret = range_search(vec, i, i+100)
+#     if ret != -1:
+#         print(i, ret)
+air_time = time.time() - start_time
+print(f"Execution Time: {air_time:.6f} seconds")
+
+print("Percentile Btre Search")
+start_time = time.time()
+# # ret = bbb.search(101)
+# # print(ret)
+for i in range(1, siz, 1):
+#     print(i)
+    ret = bbb.search(i)
+#     ret = bbb.search_range(i, i+100)
+#     if ret:
+#         print(i, ret)
+# vals = bbb.search_range(22, 90)
+air_time = time.time() - start_time
+print(f"Execution Time: {air_time:.6f} seconds")
