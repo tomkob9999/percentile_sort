b64a098314e400a30cf640e16f2980c38322d594
tokob9999 <96751911+tomkob9999@users.noreply.github.com>
2024-09-17 23:49:59 +0900

    1.3.3
    - linearization feature included

--- p_sort.py
+++ p_sort.py
@@ -4,7 +4,10 @@
 # Description: a divide and conquer sort algorithm that splits by square root percentiles instead of ranks
 # This optionally generatates a Van Emde Boas like tree that is accessible with O(loglogn) as by-product
 #
-# Version: 1.3.2
+# linearize=True preprocess by take log of input data to avoid repeating concentration during recursive partionning.  btre also contains log() values
+# 
+#
+# Version: 1.3.3
 # Author: Tomio Kobayashi
 # Last Update: 2024/9/17
 
@@ -23,14 +26,18 @@ class p_sort:
             self.len = -1
             self.min = -np.inf
             self.max = np.inf
+            self.pos1 = -1
+            self.pos2 = -1
             self.children = []
             
             self.pos1 = -1
             self.pos2 = -1
-#             self.sorted_set = None
             self.sorted_set = []
+            self.sorted_set_linear = []
 
         def search(self, v, return_node=False):
+            if self.sorted_set_linear:
+                v = np.log(v)
             return self.searchme(v, self, return_node)
         
         def searchme(self, v, bb, return_node=False):
@@ -43,7 +50,6 @@ class p_sort:
                     return bb.pos2 if not return_node else bb
                 else:
                     return -1 if not return_node else None
-            
             num_buckets = max(2, int(bb.len**(1/p_sort.ROOT_POWER)))
             ind = min(num_buckets - 1, int((v - bb.min) / (bb.max - bb.min) * num_buckets))
             if  ind < 0 or ind > len(bb.children)-1:
@@ -51,7 +57,10 @@ class p_sort:
             else:
                 return self.searchme(v, bb.children[ind], return_node)
                     
-        def link(self, sorted_vector):
+        def link(self, sorted_vector, sorted_vector_linear=[]):
+            if sorted_vector_linear:
+                tmp_linear = []
+                        
             prev = np.inf
             for i, v in enumerate(sorted_vector):
                 if prev != sorted_vector[i]:
@@ -62,25 +71,47 @@ class p_sort:
                         bb.pos2 = len(self.sorted_set)
                     prev = sorted_vector[i]
                     self.sorted_set.append(sorted_vector[i])
+                    if sorted_vector_linear:
+                        tmp_linear.append(sorted_vector_linear[i])
 
+            if sorted_vector_linear:
+                self.sorted_set_linear = tmp_linear
+                
         def search_from(self, s):
+            if self.sorted_set_linear:
+                s = np.log(s)
             suc = self.succ(s)
             if suc == -1:
                 return []
-            return self.sorted_set[suc:]
+            if self.sorted_set_linear:
+                return self.sorted_set_linear[suc:]
+            else:
+                return self.sorted_set[suc:]
         
         def search_to(self, f):
+            if self.sorted_set_linear:
+                f = np.log(f)
             pre = self.prec(f)
             if pre == -1:
                 return []
-            return self.sorted_set[:pre+1]
+            if self.sorted_set_linear:
+                return self.sorted_set_linear[:pre+1]
+            else:
+                return self.sorted_set[:pre+1]
         
         def search_range(self, s, f):
+            if self.sorted_set_linear:
+                s = np.log(s)
+                f = np.log(f)
+                
             suc = self.succ(s)
             pre = self.prec(f)
             if pre < suc or suc == -1 or pre == -1:
                 return []
-            return self.sorted_set[suc:pre+1]
+            if self.sorted_set_linear:
+                return self.sorted_set_linear[suc:pre+1]
+            else:
+                return self.sorted_set[suc:pre+1]
             
 
         def succ(self, s):
@@ -139,22 +170,35 @@ class p_sort:
                 c.append(i)
             return p_sort.sort(c, create_btre=True, link=True)
             
-    def sort(arr, create_btre=False, find_depth=True):
+    def sort(arr, create_btre=False, find_depth=True, linearize=False):
         depth = -1
         p_sort.deepest = 0
+        LOG_REP=1
         if find_depth:
             depth = 0
-            
         if create_btre:
             bb = p_sort.btre()
-            sorted_vector = p_sort.percentile_sort(arr, bb, depth=depth)
+        else:
+            bb = None
+        if linearize:
+            dat = arr
+            for _ in range(LOG_REP):
+                dat = np.log(np.array(arr) - min(arr) + 1)
+            ret, ret2 = p_sort.percentile_sort(dat, bb=bb, depth=depth, idx_vector=[], linearize=linearize)
+            
+            sorted_vector = np.array(arr)[ret2].tolist()
             if bb is not None:
-                bb.link(sorted_vector)
+                bb.link(ret, sorted_vector)
+
             return sorted_vector, bb
         else:
-            return p_sort.percentile_sort(arr, depth=depth)
+            ret = p_sort.percentile_sort(arr, bb=bb, depth=depth, idx_vector=[], linearize=linearize)
+            if bb is not None:
+                bb.link(ret)
+            return ret, bb
+            
         
-    def percentile_sort(arr, bb=None, depth=-1):
+    def percentile_sort(arr, bb=None, depth=-1, idx_vector=[], linearize=False):
         if depth > -1:
             depth += 1
             if depth > p_sort.deepest:
@@ -164,10 +208,15 @@ class p_sort:
             if bb is not None and len(arr) == 1:
                 bb.min = arr[0]
                 bb.len = 0
-            return arr
+            if linearize:
+                return arr, idx_vector
+            else:
+                return arr
         elif len(arr) == 2:
             if arr[0]>arr[1]:
                 arr[0], arr[1] = arr[1], arr[0]
+                if linearize:
+                    idx_vector[0], idx_vector[1] = idx_vector[1], idx_vector[0]
 
             if bb is not None:
                 if arr[0] == arr[1]:
@@ -176,7 +225,10 @@ class p_sort:
                     bb.min = arr[0]
                     bb.max = arr[1]
                 bb.len = 0
-            return arr
+            if linearize:
+                return arr, idx_vector
+            else:
+                return arr
 
         # Find the min and max
         min_val, max_val = min(arr), max(arr)
@@ -184,7 +236,10 @@ class p_sort:
             if bb is not None:
                 bb.min = arr[0]
                 bb.len = 0
-            return arr
+            if linearize:
+                return arr, idx_vector
+            else:
+                return arr
 
         if bb is not None:
             bb.len = len(arr)
@@ -193,18 +248,44 @@ class p_sort:
 
         num_buckets = max(2, int(len(arr)**(1/p_sort.ROOT_POWER)))
         buckets = [[] for _ in range(num_buckets)]
+        if linearize:
+            idx_buckets = [[] for _ in range(num_buckets)]
+
 
         # Insert elements into corresponding buckets based on percentile
-        for value in arr:
+        for i, value in enumerate(arr):
             # Calculate the bucket index based on the value's percentile between min and max
-            buckets[min(num_buckets - 1, int((value - min_val) / (max_val - min_val) * num_buckets))].append(value)
+            b_idx = min(num_buckets - 1, int((value - min_val) / (max_val - min_val) * num_buckets))
+            buckets[b_idx].append(value)
+            if linearize:
+                if idx_vector:
+                    idx_buckets[b_idx].append(idx_vector[i])
+                else:
+                    idx_buckets[b_idx].append(i)
+                    
         # Merge buckets
         sorted_buckets = []
-        for bucket in buckets:
-            if bb is None:
-                sorted_buckets += p_sort.percentile_sort(bucket, depth=depth)
+        if linearize:
+            sorted_buckets_idx = []
+        for i, bucket in enumerate(buckets):
+            if bb is None :
+                if linearize:
+                    sorted_bucket, sorted_bucket_idx = p_sort.percentile_sort(bucket, depth=depth, idx_vector=idx_buckets[i], linearize=linearize)
+                    sorted_buckets += sorted_bucket
+                    sorted_buckets_idx += sorted_bucket_idx
+                else:
+                    sorted_buckets += p_sort.percentile_sort(bucket, depth=depth)
             else:
                 bb.children.append(p_sort.btre())
-                sorted_buckets += p_sort.percentile_sort(bucket, bb.children[-1], depth=depth)
-                
-        return sorted_buckets
+                if linearize:
+                    sorted_bucket, sorted_bucket_idx = p_sort.percentile_sort(bucket, bb.children[-1], depth=depth, idx_vector=idx_buckets[i], linearize=linearize)
+                    sorted_buckets += sorted_bucket
+                    sorted_buckets_idx += sorted_bucket_idx
+                else:
+                    sorted_buckets += p_sort.percentile_sort(bucket, bb.children[-1], depth=depth)
+
+        if linearize:
+            return sorted_buckets, sorted_buckets_idx
+        else:
+            return sorted_buckets
+        
